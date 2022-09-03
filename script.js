be418f4b7f2b122cabc8c133033531aa3bd04d55
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-09-03 15:52:04 -0300

    commit

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-// Usar função for
-
 function JurosCompostos() {
 
-    var ValorInicial, TaxaJuros, Duracao, MensalAnual, MesAnos;
+    var ValorInicial, TaxaJuros, TaxaCalc, Duracao, MensalAnual, MesAnos;
+
+    var ValorFinal;
+
+    var parte1; var parte2; var parte3;
 
     ValorInicial = document.getElementById('valorInicial').value;
     TaxaJuros = document.getElementById('taxaJuros').value;
     Duracao = document.getElementById('duracao').value;
     MensalAnual = document.getElementById('SelectMensalAnual').value;
     MesAnos = document.getElementById('SelectMesAnos').value;
+
+    TaxaCalc = TaxaJuros/100;
 
     if(MesAnos == "Meses") {
         Duracao = Duracao;
@@ -22,6 +26,19 @@ function JurosCompostos() {
     console.log("Taxa de juros: " + TaxaJuros + "%");
     console.log("Mensal ou anual: " + MensalAnual);
     console.log("Meses ou anos: " + MesAnos);
+
+    parte1 = 1 + TaxaCalc; 
+    console.log("parte 1: " + parte1);
+
+    parte2 = Math.pow(parte1, Duracao); 
+    console.log("parte 2: " + parte2);
+
+    ValorFinal = (ValorInicial*parte2);
+    console.log("Valor final: " + ValorFinal);
+
+    // taxa de juros para calculo será a TaxaCalc
+
+    
  
 }
 
@@ -35,8 +52,9 @@ se Duracao == meses, mantem e meses OK
 se Duracao == anos, converte para meses OK
 se TaxaJuros == mensal, mantem em mensal OK
 se TaxaJuros == anual, converte para mensal OK
-MontanteFinal = MontanteInicial*[(1+TaxaJuros)^Duracao]
-Entrega MontanteFinal
+
+MontanteFinal = MontanteInicial*[(1+TaxaJuros)^Duracao] OK
+Entrega MontanteFinal Ok
  */
 
 
