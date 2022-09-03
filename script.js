2904fd06c1e8de240751b5b58d34afc6a4101b83
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-09-03 15:24:35 -0300

    Adicao das variaveis no script

--- script.js
+++ script.js
@@ -10,27 +10,31 @@ function JurosCompostos() {
     MensalAnual = document.getElementById('SelectMensalAnual').value;
     MesAnos = document.getElementById('SelectMesAnos').value;
 
+    if(MesAnos == "Meses") {
+        Duracao = Duracao;
+        console.log(Duracao + ' meses');
+    } else {
+        Duracao = Duracao * 12;
+        console.log(Duracao + ' meses');
+    }
 
-    console.log(ValorInicial);
-    console.log(TaxaJuros);
-    console.log(Duracao);
-    console.log(MensalAnual);
-    console.log(MesAnos);
-
+    console.log("Valor inicial: " + ValorInicial);
+    console.log("Taxa de juros: " + TaxaJuros + "%");
+    console.log("Mensal ou anual: " + MensalAnual);
+    console.log("Meses ou anos: " + MesAnos);
  
 }
 
 
-
-
-/* Declara MontanteInicial
-Declara TaxaJuros
-Determina se a taxa é mensal ou anual
-Declara Duracao
-se Duracao == meses, mantem e meses
-se Duracao == anos, converte para meses
-se TaxaJuros == mensal, mantem em mensal
-se TaxaJuros == anual, converte para mensal
+/*
+Declara MontanteInicial OK
+Declara TaxaJuros OK
+Determina se a taxa é mensal ou anual OK
+Declara Duracao OK
+se Duracao == meses, mantem e meses OK
+se Duracao == anos, converte para meses OK
+se TaxaJuros == mensal, mantem em mensal OK
+se TaxaJuros == anual, converte para mensal OK
 MontanteFinal = MontanteInicial*[(1+TaxaJuros)^Duracao]
 Entrega MontanteFinal
  */
