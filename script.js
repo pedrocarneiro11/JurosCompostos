75fd38671c79727bb75ee9cf8f53a22400b10477
Pedro Carneiro <pedrocarneirocunha87@gmail.com>
2023-03-14 00:47:43 -0300

    Correcao na conversao de tempo e alinhando  css

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 function JurosCompostos() {
     let valor_inicial = document.getElementById("valorInicial").value;
     let taxa = document.getElementById("taxaJuros").value;
-    let taxa_percentual_mensal = taxa / 100;
+    let taxa_percentual = taxa / 100;
     let duracao = document.getElementById("duracao").value;
     let mesAnos = document.getElementById("SelectMesAnos").value; //Meses ou Anos  
+    let mensalAnual = document.getElementById("SelectMensalAnual").value; //Mensal ou Anual  
     let valor_final
 
     if (mesAnos=="Meses") {
         console.log("1")
-    } else if (mesAnos=="Anos") {
-        console.log("2")
+    } else if (mesAnos=="Anos" && mensalAnual == "Mensal") {
+        duracao = duracao * 12
     }
 
-    valor_final = valor_inicial * Math.pow(1 + taxa_percentual_mensal, duracao);
+    if (mensalAnual=="Mensal") {
+        console.log("2")
+    } else if (mensalAnual=="Anual") {
+        if (mesAnos == "Meses") {
+            duracao = duracao / 12
+        }
+    }
+
+    valor_final = valor_inicial * Math.pow(1 + taxa_percentual, duracao);
 
     console.log(valor_inicial);
     console.log(taxa);
@@ -20,5 +29,5 @@ function JurosCompostos() {
     console.log(valor_final); 
     console.log(mesAnos); 
 
-    document.getElementById("resultado").innerHTML= valor_final;
+    document.getElementById("resultado").innerHTML= valor_final.toFixed(2);
   }
